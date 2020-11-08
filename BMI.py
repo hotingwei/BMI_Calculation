70e862afea094314792741a1d7930a938470ebff
hotingwei <anitaho577@gmail.com>
2020-11-08 21:14:08 +0100

    Error Correction

--- BMI.py
+++ BMI.py
@@ -7,13 +7,13 @@ BMI = weight / height / height
 
 if BMI < 18.5:
     print('您的BMI為', BMI, '屬體重過輕')
-elif BMI <= 18.5 and BMI < 24:
+elif BMI >= 18.5 and BMI < 24:
     print('您的BMI為', BMI, '屬正常')
-elif BMI <= 24 and BMI < 27:
+elif BMI >= 24 and BMI < 27:
     print('您的BMI為', BMI, '屬過重')
-elif BMI <= 27 and BMI < 30:
+elif BMI >= 27 and BMI < 30:
     print('您的BMI為', BMI, '屬輕度肥胖')
-elif BMI < 30 and BWI < 35:
+elif BMI > 30 and BWI < 35:
 	print('您的BMI為', BMI, '屬中度肥胖')
 else:
 	print('您的BMI為', BMI, '屬重度肥胖')
